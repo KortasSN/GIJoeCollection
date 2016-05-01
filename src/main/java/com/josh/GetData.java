788630b80ed6a3e0f7dc0364bb0d33aeb62fa20c
Destro <kortas@gmail.com>
2016-04-30 20:45:50 -0500

    Pulls Data From Excel File

---
+++ src/main/java/com/josh/GetData.java
@@ -0,0 +1,17 @@
+package com.josh;
+
+/**
+ * Created by Destro on 4/30/2016.
+ */
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class GetData {
+
+
+
+}
